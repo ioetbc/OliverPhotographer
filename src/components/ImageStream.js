e8c6dae8b0d7139d934933fdc6d4c80fc2a6dcee
William Cole <ioetbc@gmail.com>
2019-04-21 23:18:48 +0100

    more compatible lazy loading

--- src/components/ImageStream.js
+++ src/components/ImageStream.js
@@ -28,25 +28,42 @@ import Bricks from 'bricks.js'
         window.addEventListener('resize', function() {
             bricks.pack();
         });
-        window.addEventListener("DOMContentLoaded", function() {
-            var lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
-            if ("IntersectionObserver" in window) {
-                let lazyImageObserver = new IntersectionObserver(function(entries, observer) {
-                    entries.forEach(function(entry) {
-                        if (entry.isIntersecting) {
-                        let lazyImage = entry.target;
-                        lazyImage.src = lazyImage.dataset.src;
-                        lazyImage.srcset = lazyImage.dataset.srcset;
-                        lazyImage.classList.remove("lazy");
-                        lazyImageObserver.unobserve(lazyImage);
-                        }
-                    });
-                });
-                lazyImages.forEach(function(lazyImage) {
-                    lazyImageObserver.observe(lazyImage);
-                });
-            }
-        });
+        document.addEventListener("DOMContentLoaded", function() {
+            let lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
+            let active = false;
+
+            const lazyLoad = function() {
+              if (active === false) {
+                active = true;
+
+                setTimeout(function() {
+                  lazyImages.forEach(function(lazyImage) {
+                    if ((lazyImage.getBoundingClientRect().top <= window.innerHeight && lazyImage.getBoundingClientRect().bottom >= 0) && getComputedStyle(lazyImage).display !== "none") {
+                      lazyImage.src = lazyImage.dataset.src;
+                      lazyImage.srcset = lazyImage.dataset.srcset;
+                      lazyImage.classList.remove("lazy");
+
+                      lazyImages = lazyImages.filter(function(image) {
+                        return image !== lazyImage;
+                      });
+
+                      if (lazyImages.length === 0) {
+                        document.removeEventListener("scroll", lazyLoad);
+                        window.removeEventListener("resize", lazyLoad);
+                        window.removeEventListener("orientationchange", lazyLoad);
+                      }
+                    }
+                  });
+
+                  active = false;
+                }, 200);
+              }
+            };
+
+            document.addEventListener("scroll", lazyLoad);
+            window.addEventListener("resize", lazyLoad);
+            window.addEventListener("orientationchange", lazyLoad);
+          });
     };
 
     render() {
@@ -59,7 +76,7 @@ import Bricks from 'bricks.js'
                     {images.map((image, i) => (
                         <img
                             id={'image-stream-' + i}
-                            className="grid-item lazy"
+                            className="lazy grid-item"
                             style={{ height: image.height, cursor: 'pointer' }}
                             onClick={() => handleSlideShow(i, image.optimized,)}
                             src={require(`../images/placeholder/${image.placeholder}`)}
